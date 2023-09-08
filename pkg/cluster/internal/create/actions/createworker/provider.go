a4f1c80b3b7eb3d7730cbc6795b6063769c6c3e0
lreciomelero <lrecio@stratio.com>
2023-09-08 10:57:54 +0200

    added firstInstallation for secret keoscluster-registries

--- pkg/cluster/internal/create/actions/createworker/provider.go
+++ pkg/cluster/internal/create/actions/createworker/provider.go
@@ -51,8 +51,8 @@ const (
 
 	scName = "keos"
 
-	keosClusterChart = "0.1.0-M4"
-	keosClusterImage = "0.1.0-SNAPSHOT"
+	keosClusterChart = "0.1.0-PR50-SNAPSHOT"
+	keosClusterImage = "0.1.0-PR50-SNAPSHOT"
 )
 
 const machineHealthCheckWorkerNodePath = "/kind/manifests/machinehealthcheckworkernode.yaml"
@@ -282,7 +282,7 @@ func deployClusterOperator(n nodes.Node, keosCluster commons.KeosCluster, cluste
 	}
 
 	// Create the docker registries credentials secret for keoscluster-controller-manager
-	if clusterCredentials.DockerRegistriesCredentials != nil {
+	if clusterCredentials.DockerRegistriesCredentials != nil && firstInstallation {
 		jsonDockerRegistriesCredentials, err := json.Marshal(clusterCredentials.DockerRegistriesCredentials)
 		if err != nil {
 			return errors.Wrap(err, "failed to marshal docker registries credentials")
